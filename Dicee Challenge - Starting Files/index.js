ac15d8e464f7e75bcbd2da53bb37717211e0d515
Nishtha Jain <81468976+Nishtha18-nj@users.noreply.github.com>
2023-03-10 11:45:26 +0530

    Add files via upload

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,26 @@
+
+// IMAGE 1 
+var randomNumber1 = Math.floor(Math.random() * 6)+1;
+var randomDiceImage = "images/dice" + randomNumber1 + ".png" ;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomDiceImage );
+
+
+// IMAGE 2
+var randomNumber2 = Math.floor(Math.random() * 6)+1;
+var randomDiceImage2 = "images/dice" +randomNumber2 +".png";
+var image2= document.querySelectorAll("img")[1];
+image2.setAttribute("src" , randomDiceImage2);
+
+// Text on the top 
+if (randomNumber1>randomNumber2)
+{
+    document.querySelector("h1").innerHTML="Play 1 wins! ";
+}
+else if (randomNumber1<randomNumber2)
+{
+    document.querySelector("h1").innerHTML="Play 2 wins! ";
+}
+else {
+    document.querySelector("h1").innerHTML ="It's a draw!";
+}
